df1040edf778af48af4793a07118ff2059c94e0a
smilejakdu <ash982416@gmail.com>
2020-06-29 14:49:30 +0900

    excel_url_modify

--- excel/urls.py
+++ excel/urls.py
@@ -6,7 +6,7 @@ from .views      import (ExcelView ,
 
 urlpatterns = [
     path("upload"                            , ExcelView.as_view()),
+    path("statistics"                        , StatisticsPage.as_view()),
     path("<str:excel_name>"                  , ExcelDetailView.as_view()),
     path("<str:excel_name>/<str:sheet_name>" , SheetDetailView.as_view()),
-    path("statistics"                        , StatisticsPage.as_view())
 ]
