52f81ab135db8036e19ad0ffebe2923c3d4b542c
smilejakdu <ash982416@gmail.com>
2020-06-29 14:37:47 +0900

    statistics_urls

--- excel/urls.py
+++ excel/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views      import (ExcelView ,
                          ExcelDetailView,
-                         SheetDetailView)
+                         SheetDetailView,
+                         StatisticsPage)
 
 urlpatterns = [
     path("upload"                            , ExcelView.as_view()),
     path("<str:excel_name>"                  , ExcelDetailView.as_view()),
     path("<str:excel_name>/<str:sheet_name>" , SheetDetailView.as_view()),
+    path("statistics"                        , StatisticsPage.as_view())
 ]
