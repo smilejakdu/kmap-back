69436731f73c09b4d96f290fe763d6dd41df2522
smilejakdu <ash982416@gmail.com>
2020-06-29 17:28:02 +0900

    columns_list_error

--- excel/views.py
+++ excel/views.py
@@ -226,7 +226,7 @@ class StatisticsPage(View):
             # Total KMAP-2K Profile Numbers
             # 딕셔너리로 줘야하나 ??
             columns_list = [
-                {"name" : previous_day_two , "value" : len(previous_day_two_list)}
+                {"name" : previous_day_two , "value" : len(previous_day_two_list)},
                 {"name" : previous_day_one , "value" : len(previous_day_one_list)},
                 {"name" : now_day,"value":len(now_list)}]
 
