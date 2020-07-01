bd250f8505383e65619c7c04a5f51f640d3c4e3a
smilejakdu <ash982416@gmail.com>
2020-07-01 17:27:50 +0900

    compound_data

--- compound/urls.py
+++ compound/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from .views   import CompoundView
 
 urlpatterns = [
+    path("<int:chem_index>" , CompoundView.as_view()),
 ]
--- compound/views.py
+++ compound/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.views import View
+from django.http  import HttpResponse , JsonResponse
+from .models      import Compound
 
-# Create your views here.
+
+class CompoundView(View):
+    def get(self , request , chem_index):
+        print(chem_index)
+        if not chem_index:
+            return JsonResponse({"message" : "DOES_NOT_CHEM_INDEX"} , status=400)
+
+        try :
+            compound_info = (Compound.
+                             objects.
+                             filter(kaipharm_chem_index = chem_index).
+                             values("subset",
+                                    "japan",
+                                    "europe",
+                                    "usa",
+                                    "nci_cancer",
+                                    "kaichem_id",
+                                    "kaipharm_chem_index",
+                                    "chem_series",
+                                    "chem_series_cid",
+                                    "compound",
+                                    "cid",
+                                    "inchikey",
+                                    "pubchem_name",
+                                    "ipk",
+                                    "prestwick",
+                                    "selleckchem",
+                                    "known_target",
+                                    "information"))
+
+            return JsonResponse({"data" : list(compound_info)} , status=200)
+
+        except Compound.DoesNotExist:
+            return JsonResponse({"message" : "DOES_NOT_COMPOUND"} , status=400)
+
+        except TypeError:
+            return JsonResponse({"message" : "INVALID_TYPE"}, status=400)
+
+        except Exception as e:
+            return JsonResponse({"message" : e} , status=400)
+
