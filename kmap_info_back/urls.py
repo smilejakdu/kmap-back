8437f08e1ef613ecad369e222a9bee90f4d6f41e
smilejakdu <39209673+smilejakdu@users.noreply.github.com>
2021-02-04 16:22:50 +0900

    Delete kmap_info_back directory

--- kmap_info_back/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path , include
-
-urlpatterns = [
-    path("excel/"    , include("excel.urls")),
-    path("account/"  , include("account.urls")),
-    path("compound/" , include("compound.urls")),
-]
